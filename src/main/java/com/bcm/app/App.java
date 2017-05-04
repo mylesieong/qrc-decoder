2c5638b66e8f8324781e64091f220115055dbf86
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2017-05-04 15:34:25 +0800

    mvp

--- src/main/java/com/bcm/app/App.java
+++ src/main/java/com/bcm/app/App.java
@@ -2,32 +2,53 @@ package com.bcm.app;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class App {
+
     public final static String COMMAND = "parseQRC ";
+
     public static void main( String[] args ){
-	if (args.length != 1){
-	    return ;
-	}
-	String imageFile = args[0];
-        App app = new App();
-	String output = app.executeCommand(COMMAND + imageFile);
-	System.out.println(output);
+        if (args.length != 1){
+            return ;
+        }
+        String imageFile = args[0];
+        String output = executeCommand(COMMAND + imageFile);
+        String[] outputTokens = tokenizeOutput(output);
+        for (int i = 0; i < outputTokens.length ; i++){
+            System.out.print(outputTokens[i] + "\t");
+        }
     }
-    private String executeCommand(String command){
+
+    private static String executeCommand(String command){
         StringBuffer output = new StringBuffer();
-	Process process;
-	try {
-	    process = Runtime.getRuntime().exec(command);
-	    process.waitFor();
-	    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-	    String line = "";
-	    while((line = reader.readLine()) != null){
+        Process process;
+        try {
+            process = Runtime.getRuntime().exec(command);
+            process.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = "";
+            while((line = reader.readLine()) != null){
                 output.append(line + "\n");
-	    }
-	}catch (Exception e){
+            }
+        }catch (Exception e){
             e.printStackTrace();
-	}
-	return output.toString();
+        }
+        return output.toString();
     }
+
+    private static String[] tokenizeOutput(String output){
+        String[] outputTokenized = output.split(";");
+        ArrayList<String> helperList = new ArrayList<String>();
+        for (int i = 0 ; i < 6 ; i++){
+            if (outputTokenized[i] != null){
+                helperList.add(outputTokenized[i]);
+            }else{
+                helperList.add("");
+            }
+        }
+        String[] result = new String[6];
+        return helperList.toArray(result);
+    }
+    
 }
