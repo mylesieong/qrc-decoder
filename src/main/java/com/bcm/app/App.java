de2b70a7dc1b7b8ed008482a9bcea9c6661f7041
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2017-05-04 14:27:07 +0800

    Build Runtime call command, Runnable

--- src/main/java/com/bcm/app/App.java
+++ src/main/java/com/bcm/app/App.java
@@ -1,13 +1,29 @@
 package com.bcm.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class App {
+    public static void main( String[] args ){
+        App app = new App();
+	String command = "ping -n 3 www.google.com";
+	String output = app.executeCommand(command);
+	System.out.println(output);
+    }
+    private String executeCommand(String command){
+        StringBuffer output = new StringBuffer();
+	Process process;
+	try {
+	    process = Runtime.getRuntime().exec(command);
+	    process.waitFor();
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+	    String line = "";
+	    while((line = reader.readLine()) != null){
+                output.append(line + "\n");
+	    }
+	}catch (Exception e){
+            e.printStackTrace();
+	}
+	return output.toString();
     }
 }
