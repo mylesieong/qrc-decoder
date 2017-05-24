fec99139837c9391f8e9e36bb27f8820e0405093
U-HQ_DSB\bi77 <bi77@pc-11-032.corp.dahsing.com>
2017-05-24 14:45:41 +0800

    1. File format updates ^M 2. Change the engine of Convertor.java to ImageMagick

--- src/main/java/com/bcm/app/Convertor.java
+++ src/main/java/com/bcm/app/Convertor.java
@@ -3,13 +3,18 @@ package com.bcm.app;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.io.File;
 
 public class Convertor {
 
-    public final static String COMMAND = "java -jar pdfbox-app-2.0.5.jar PDFToImage ";
+    //public final static String COMMAND = "java -jar pdfbox-app-2.0.5.jar PDFToImage ";
+    public final static String COMMAND = "./imagemagick_portable/convert -density 240 -quality 80 -trim ";
 
     public static void convertPDFToImage( String filename){
-        executeCommand(COMMAND + filename);
+        //executeCommand(COMMAND + filename);
+        String path = filename.substring(0, filename.lastIndexOf(File.separator)); 
+        String imagename = path + File.separator + "temp.jpg";
+        executeCommand(COMMAND + filename + " " + imagename);
     }
 
     private static String executeCommand(String command){
