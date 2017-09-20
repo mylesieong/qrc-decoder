1ff8f7a617448e98ec209abc25dad343766c7c1c
mylesieong <myles.bcm@gmail.com>
2017-09-20 09:28:29 +0800

    Update final jar name and update the entrance batch

--- qrc-decoder.bat
+++ qrc-decoder.bat
@@ -3,4 +3,4 @@ rem jre7\bin\java -jar target\qrc-decoder-1.0-SNAPSHOT-shaded.jar
 rem version 1.2
 rem jre7\bin\java -jar target\qrc-decoder-1.2-shaded.jar
 rem version 1.2r0
-jre7\bin\java -jar target\qrc-decoder-1.2r0-shaded.jar
+jre7\bin\java -jar target\qrc-decoder.jar
